d9a7bb53af3aadd4083ed6eede1f835bfa1f3269
Sulthanul Faris <84757988+Sulthan-Sulthan@users.noreply.github.com>
2026-03-16 17:49:57 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+from functions.downlaod_api import download_audio_api
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or ["http://localhost:3000"] for security
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["Content-Disposition"] 
+)
+
+@app.get("/download")
+def download_audio(link: str = Query(...)):
+    return download_audio_api(link)
