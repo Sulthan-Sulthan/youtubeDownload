08f53dad996ef445982e9968b8dcdcda4b5bc86b
Sulthanul Faris <84757988+Sulthan-Sulthan@users.noreply.github.com>
2026-03-24 11:14:22 +0530

    Fix import statement for download_api module

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
-from functions.downlaod_api import download_audio_api, download_video_api
+from downlaod_api import download_audio_api, download_video_api
 
 app = FastAPI()
 
