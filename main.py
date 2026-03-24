748f8ff769259cb97827c33f258cac3330651415
Sulthanul Faris <84757988+Sulthan-Sulthan@users.noreply.github.com>
2026-03-24 11:08:29 +0530

    added video

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
-from downlaod_api import download_audio_api
+from functions.downlaod_api import download_audio_api, download_video_api
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # or ["http://localhost:3000"] for security
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -16,3 +16,10 @@ app.add_middleware(
 @app.get("/download")
 def download_audio(link: str = Query(...)):
     return download_audio_api(link)
+
+@app.get("/download-video")
+def download_video(link: str = Query(...)):
+    return download_video_api(link)
+
+
+    # run cmd  ------------>  uvicorn main:app --reload --port 8080
